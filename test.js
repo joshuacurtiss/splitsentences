ad2032d5385f6b4d51668e0008228a7c45ae94d9
Josh Curtiss <git@curtiss.me>
2019-03-13 11:48:31 -0500

    Improvements to test.
    - Use lorem ipsum.
    - Use actual splitSentences name for function.

--- test.js
+++ test.js
@@ -1,34 +1,32 @@
-const splitStrings = require('./index');
+const splitSentences = require('./index');
 const article = `
-    The Industrial Revolution had several roots, one of which was a commercial revolution that, beginning as far back as the sixteenth century, accompanied Europe’s expansion overseas.
-    Both exports and imports showed spectacular growth, particularly in England and France.
-    An increasingly larger portion of the stepped-up commercial activity was the result of trade with overseas colonies.
-    Imports included a variety of new beverages, spices, and ship’s goods around the world and brought money flowing back.
-    Europe’s economic institutions, particularly those in England, were strong, had wealth available for new investment, and seemed almost to be waiting for some technological breakthrough that would expand their profit-making potential even more.
-    The breakthrough came in Great Britain, where several economic advantages created a climate especially favorable to the encouragement of new technology.
-    One was its geographic location at the crossroads of international trade. Isn't that great? 
-    Internally, Britain was endowed with easily navigable natural waterway, which helped its trade and communication with the world.
-    Beginning in the 1770’s, it enjoyed a boom in canal building, which helped make its domestic market more accessible.
-    Because water transportation was the cheapest means of carrying goods to market, canals reduced prices and thus increased consumer demand.
-    Great Britain also had rich deposits of coal that fed the factories springing up in industrial and consumer goods.
-    Another advantage was Britain’s large population of rural, agricultural wage earners,as well as cottage workers, who had the potential of being more mobile than peasants of some other countries.
-    Eventually they found their way to the cities or mining communities and provided the human power upon which the Industrial Revolution was built.
-    The British people were also consumers; the absence of internal tariffs, such as those that existed in France or Italy or between the German states, made Britain the largest free-trade area in Europe.
-    Britain’s relatively stable government also helped create an atmosphere conducive to industrial progress.
-    Great Britain’s better-developed banking and credit system also helped speed the industrial progress, as did the fact that it was the home of an impressive array of entrepreneurs and inventors.
-    Among them were a large number of nonconformists whose religious principles encouraged thrift and industry rather than luxurious living and who tended to pour their profits back into their business thus providing the basis for continued expansion and other fun stuff that they would like to do I would imagine although I am guessing a little bit in this case, for sure.
-    A precursor to the Industrial Revolution was a revolution in agricultural techniques.
-    Ideas about agricultural reform developed first in Holland, where as early as the mid-seventeenth century, such modern methods as crop rotation, heavy fertilization, and diversification were all in use.
-    Dutch peasant farmers were known throughout Europe for their agricultural innovations, but as British markets and opportunities grew, the English quickly learned from them.
-    As early as the seventeenth century the Dutch were helping them drain marshes and fens where, with the help of advanced techniques, they grew new crops.
-    By the mid-eighteenth century new agricultural methods as well as selective breeding of livestock had caught on throughout the country.
-    Much of the increased production was consumed by Great Britain’s burgeoning population.
-    At the same time, people were moving to the city, partly because of the enclosure movement; that is, the fencing of common fields and pastures in order to provide more compact, efficient privately held agricultural parcels that would produce more goods and greater profits.
-    In the sixteenth century enclosures were usually used for creating sheep pastures, but by the eighteenth century new farming techniques made it advantageous for large landowners to seek enclosures in order to improve agricultural production.
-    Between 1714 and 1820 over 6 million acres of English land were enclosed.
-    As a result, many small, independent farmers were forced to sell out simply because they could not compete.
-    Non-landholding peasants and cottage workers, who worked for wages and grazed cows or pigs on the village common, were also hurt when the common was no longer available.
-    It was such people who began to flock to the cities seeking employment and who found work in the factories that would transform the nation and, the world.
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla rhoncus eros vitae mi placerat placerat. Nunc quis 
+    placerat nulla. Praesent mattis sapien euismod, suscipit risus vitae, porttitor arcu. In hac habitasse platea 
+    dictumst. Duis non ipsum eu nulla viverra ultricies eget ac sem. Nam viverra et leo quis scelerisque. Nullam 
+    vehicula mollis metus sed ullamcorper. Quisque molestie tortor a mollis tempus. Nam facilisis leo at quam iaculis, 
+    vitae pretium ligula eleifend! Etiam in quam ultricies, posuere dui sed, tristique magna. Proin varius nec leo at 
+    pretium. Cras vitae enim sit amet lectus ullamcorper vestibulum vitae a nisi. Phasellus nisl magna, cursus ut 
+    finibus at, varius non lectus.
+
+    Nunc sapien risus, aliquet in tempus eu, efficitur et tortor. Suspendisse molestie, lorem et dictum vehicula, 
+    purus ex faucibus diam, sed tincidunt justo metus eu ipsum. Suspendisse eget dolor ex. Phasellus consectetur, eros 
+    et mattis egestas, diam lorem tempus nulla, consequat aliquet sem ligula non nulla. Morbi sit amet facilisis nunc, 
+    ac tristique neque. Integer ac aliquam orci. Curabitur posuere ultrices luctus. Curabitur cursus diam lectus, eget 
+    maximus ipsum aliquet vel. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; 
+    In orci est, venenatis in arcu eu, sollicitudin vehicula dolor. Proin condimentum augue molestie, tristique dui 
+    eget, hendrerit libero. Nulla facilisi. Vestibulum vestibulum lorem eget dignissim porttitor.
+
+    Lorem ipsum dolor sit amet consectetur adipiscing elit nulla rhoncus eros vitae mi placerat placerat nunc quis, 
+    placerat nulla praesent mattis sapien euismod suscipit risus vitae, porttitor arcu in hac habitasse platea 
+    dictumst duis non ipsum eu nulla viverra ultricies eget ac sem nam viverra et leo quis scelerisque nullam 
+    vehicula mollis metus sed ullamcorper quisque molestie tortor a mollis tempus nam facilisis leo at quam iaculis
+    vitae pretium ligula eleifend.
+    
+    Ut egestas mollis diam, ut bibendum sem blandit quis. Curabitur lobortis, dui ut mollis hendrerit, risus arcu 
+    luctus lectus, in suscipit justo dui luctus magna. Suspendisse ut sem consectetur ipsum dignissim commodo eget in 
+    neque. Vivamus eget vulputate velit. Quisque at velit non risus consequat rhoncus at sed tortor. Aenean efficitur 
+    diam massa, eu aliquet lacus dapibus ut. Etiam elementum massa id augue consequat maximus. Nunc fringilla nunc at 
+    venenatis iaculis. Nullam sagittis vitae nisi at tempus. Ut mattis urna at tortor.
 `;
-const strings = splitStrings(article, 199);
+const strings = splitSentences(article, 199);
 console.log(strings);
